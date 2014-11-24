ce4be7a230ba09dbd8d381f04991720068747deb
doz <dozray@users.noreply.github.com>
2014-11-24 15:07:43 +0800

    更新商品列表页样式

--- themes/default/_js/_category.js
+++ themes/default/_js/_category.js
@@ -27,7 +27,7 @@ $j(function() {
 	} else {
 		$("._sub-cat").css("background-image", "none");
 	}
-	
+
 	var _btns = $j("._btns input");
 	_btns.click(function() {
 		_btns.css({"background":"#fff","color":"#8fc31f"});
@@ -37,17 +37,26 @@ $j(function() {
 	
 	$j(".list-goods").hover(function() {
 		var _this = $j(this);
-		_this.css({"box-shadow":"0px 0px 10px #ccc"});
-		_this.find("._addtocart").css({"background":"#8fc31f", "color":"#fff"});
-		_this.find(".decr").css({"background-image":"url(themes/default/_images/j_08.png)","background-color":"#f6643d"});
-		_this.find(".incr").css({"background-image":"url(themes/default/_images/j_03.png)","background-color":"#8fc31f"});
+		_this.find("._list_goods_bg").show();
+		_this.find("._addtocart").css({"background":"#f6643d", "color":"#fff"});
+		/*_this.find(".decr").css({"background-image":"url(themes/default/_images/j_08_0.png)","background-color":"#f6643d"});
+		_this.find(".incr").css({"background-image":"url(themes/default/_images/j_05_0.png)","background-color":"#8fc31f"});*/
+		_this.find(".decr").css({"background-image":"url(themes/default/_images/j_08_0.png)"});
+		_this.find(".incr").css({"background-image":"url(themes/default/_images/j_05_0.png)"});
+		_this.find("._dgi_con").css("background", "#b8d681");
+		_this.find(".goods-numb").css({"background":"#aecd74", "box-shadow":"0px 3px 3px #689547 inset", "border-color":"#9fc16c", "color":"#fff"});
 	}, function() {
 		var _this = $j(this);
-		_this.css({"box-shadow":"none"});
-		_this.find("._addtocart").css({"background":"#fff", "color":"#8fc31f"});
-		_this.find(".decr").css({"background-image":"url(themes/default/_images/j_11.png)","background-color":"#fff"});
-		_this.find(".incr").css({"background-image":"url(themes/default/_images/j_05.png)","background-color":"#fff"});
+		_this.find("._list_goods_bg").hide();
+		_this.find("._addtocart").css({"background":"#fff", "color":"#f6643d"});
+		/*_this.find(".decr").css({"background-image":"url(themes/default/_images/j_11_0.png)","background-color":"#fff"});
+		_this.find(".incr").css({"background-image":"url(themes/default/_images/j_03_0.png)","background-color":"#fff"});*/
+		_this.find(".decr").css({"background-image":"url(themes/default/_images/j_11_0.png)"});
+		_this.find(".incr").css({"background-image":"url(themes/default/_images/j_03_0.png)"});
+		_this.find("._dgi_con").css("background", "#fff");
+		_this.find(".goods-numb").css({"background":"#fff", "box-shadow":"none", "border-color":"#b8d681", "color":"#a2a2a2"});
 	});
+	
 	
 	//alert(navigator.userAgent);
 });
